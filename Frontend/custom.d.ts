84126251577452c45746e29b924f6df287314bb4
Jhonatan Arevalo <jhon20339@gmail.com>
2025-01-23 16:32:46 -0500

    Fronted: Configurar el archivo container

--- Frontend/custom.d.ts
+++ Frontend/custom.d.ts
@@ -2,3 +2,27 @@ declare module "*.svg" {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
     export default content;
   }
+  declare module '*.jpg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.png' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.gif' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.svg' {
+    const content: string;
+    export default content;
+  }
--- Frontend/src/components/container.tsx
+++ Frontend/src/components/container.tsx
@@ -1,1 +1,23 @@
 import React from 'react'
+import './Container.scss'
+import img1 from '../assets/image1.jpg'
+import img2 from '../assets/image2.png'
+import img3 from '../assets/image3.png'
+import img4 from '../assets/image4.png'
+import img5 from '../assets/image5.jpg'
+import img6 from '../assets/image6.png'
+
+const Container = () => {
+  return (
+    <div className="container">
+      <img src={img1} alt="" />
+      <img src={img2} alt="" />
+      <img src={img3} alt="" />
+      <img src={img4} alt="" />
+      <img src={img5} alt="" />
+      <img src={img6} alt="" />
+    </div>
+  )
+}
+
+export default Container
